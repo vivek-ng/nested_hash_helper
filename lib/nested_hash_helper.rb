9fb405b830af46eeef56c925d487cbc3a913bedd
vivek n <vivekn@freshdesk.com>
2017-03-03 18:41:55 +0530

    added methods for deep_delete_empty

--- lib/nested_hash_helper.rb
+++ lib/nested_hash_helper.rb
@@ -14,6 +14,20 @@ def deep_except(*excluded_keys)
   	    end
   	end
   	end
+
+def deep_delete_empty
+	current_class = self.class
+	self.each do |current_keys , current_value|
+       if current_value.nil? || current_value.empty?
+       	  self.delete(current_keys)
+       	  next
+       end	
+       if current_value.is_a?(current_class)
+       	    current_value.deep_delete_empty(*excluded_keys)
+       end	
+	end
+end
+
 end
 
 
